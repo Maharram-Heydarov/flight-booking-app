c4a22c9577dae79b7f3d971554de004a8658e65a
Maharram <meherremheyderov04@gmail.com>
2025-01-26 16:33:43 +0400

    Initial commit

---
+++ src/test/java/az/edu/turing/flightbookingapp/FlightBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flightbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
