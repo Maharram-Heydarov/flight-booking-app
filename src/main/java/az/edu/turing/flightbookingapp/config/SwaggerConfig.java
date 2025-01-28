671a6eeaba136881a0d4539d92e3dac5340e8c3f
Maharram <meherremheyderov04@gmail.com>
2025-01-28 23:02:05 +0400

    add configuration and swaggerConfig

--- src/main/java/az/edu/turing/flightbookingapp/config/SwaggerConfig.java
+++ src/main/java/az/edu/turing/flightbookingapp/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package az.edu.turing.flightbookingapp.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Flight Booking Application API")
+                        .version("1.0.0")
+                        .description("This API provides functionalities for flight booking application, including user authentication, flight management, and booking operations."));
+    }
 }
